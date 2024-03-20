9a1364b8648421565a30b08ba863ffb92ca36b28
直直波浪线 <1239216438@qq.com>
2024-03-20 23:07:57 +0800

    修改文件编码，解决乱码问题

--- QBLearnOpenGL/QBLearnOpenGL/main.cpp
+++ QBLearnOpenGL/QBLearnOpenGL/main.cpp
@@ -1,65 +1,66 @@
+﻿//第一次提交，欧耶！！！
 #include <glad/glad.h>
 #include <glfw3.h>
 #include <iostream>
-//ע��ص������������ڴ�С������ʱ����
+//注册回调函数，当窗口大小被调整时调用
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 void processInput(GLFWwindow *window);
 
 int main()
 {
-	//��ʼ��glfw
+	//初始化glfw
 	glfwInit();
-	//����OpenGL���汾�ʹΰ汾�Ŷ�Ϊ3
+	//设置OpenGL主版本和次版本号都为3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
-	//ʹ�õ��Ǻ���ģʽ(Core-profile) ��ֻ��ʹ��OpenGL���ܵ�һ���Ӽ���û�������Ѳ�����Ҫ�����������ԣ�
+	//使用的是核心模式(Core-profile) ，只能使用OpenGL功能的一个子集（没有我们已不再需要的向后兼容特性）
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// Mac os xϵͳר��
+	// Mac os x系统专用
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	//�������崰�� ���������ڿ������ڸߣ����ڱ��⣬��ʱ���ԣ���ʱ����
+	//创建整体窗口 参数：窗口宽，窗口高，窗口标题，暂时忽略，暂时忽略
 	GLFWwindow* window = glfwCreateWindow(800,600,"LearnOpendGL",NULL,NULL);
 	if (window == NULL)
 	{
-		std::cout << "����GLFW����ʧ�ܣ���" << std::endl;
-		//�����߳�
+		std::cout << "创建GLFW窗口失败！！" << std::endl;
+		//结束线程
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
 
-	//��ʼ��GLAD������OpenGL�ĺ���ָ��
+	//初始化GLAD来管理OpenGL的函数指针
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
-		std::cout << "��ʼ�� GLAD ʧ�ܣ���" << std::endl;
+		std::cout << "初始化 GLAD 失败！！" << std::endl;
 		return -1;
 	}
 
-	//��Ⱦ���ڣ���Ⱦ���ڿ���С�����崰�ڣ� ���������½�λ�ã�X��Y����������
+	//渲染窗口（渲染窗口可以小于整体窗口） 参数：左下角位置（X，Y），宽，高
 	glViewport(0, 0, 800, 600);
 
-	//��������ص�
+	//设置拉伸回调
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	//�������û�б��ر����ѭ����Ⱦ����
+	//如果窗口没有被关闭则会循环渲染窗口
 	while (!glfwWindowShouldClose(window))
 	{
-		//�������
+		//按键监控
 		processInput(window);
 
-		//��Ⱦָ��TODO
+		//渲染指令TODO
 		
-		glClearColor(0.2f,0.3f,0.3f,1.0f);	  //��Ⱦ��ɫ
-		glClear(GL_COLOR_BUFFER_BIT);//�����һ֡����ɫ
+		glClearColor(0.2f,0.3f,0.3f,1.0f);	  //渲染颜色
+		glClear(GL_COLOR_BUFFER_BIT);//清除上一帧的颜色
 
-		//�����ύ����ɫ���壨����һ��������GLFW����ÿһ��������ɫֵ�Ĵ󻺳壩��������һ�����б��������ƣ����ҽ�����Ϊ�����ʾ����Ļ�ϡ�
+		//函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
 		glfwSwapBuffers(window);
-		//���������û�д���ʲô�¼�������������롢����ƶ��ȣ������´���״̬�������ö�Ӧ�Ļص�����������ͨ���ص������ֶ����ã���
+		//函数检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）。
 		glfwPollEvents();
 	}
 
-	//����Ⱦѭ���������Ƿ�������Դ
+	//当渲染循环结束后是否所有资源
 	glfwTerminate();
 
 	return 0;
@@ -67,13 +68,13 @@ int main()
 
 void  framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-	//������ʱ������Ⱦ�����Ĵ���
+	//被拉伸时重新渲染拉伸后的窗口
 	glViewport(0, 0, width, height);
 }
 
 void processInput(GLFWwindow *window)
 {
-	//�������ESC����رճ���
+	//如果按下ESC键则关闭程序
 	if (glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window,true);
