3ab051cb5efbbcb7e454106b2ec5de889968ebdc
直直波浪线 <1239216438@qq.com>
2024-03-27 22:59:02 +0800

    OpenGL学习完成三角绘制！！

--- QBLearnOpenGL/02_hello_triangle/main.cpp
+++ QBLearnOpenGL/02_hello_triangle/main.cpp
@@ -7,12 +7,21 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 void processInput(GLFWwindow *window);
 
+  //顶点着色器程序
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
 "}\0";
+
+//片段着色器程序
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{"
+"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}";
 
 int main()
 {
@@ -58,9 +67,13 @@ int main()
 	 0.0f,  0.5f, 0.0f
 	};
 
-	//创建VBO（顶点缓冲对象）
-	unsigned int VBO;
+	//创建VBO（顶点缓冲对象）、VAO（顶点数组对象）
+	unsigned int VBO, VAO;
 	glGenBuffers(1,&VBO);
+	glGenVertexArrays(1,&VAO);
+
+	//绑定VAO对象
+	glBindVertexArray(VAO);
 
 	//绑定缓冲
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -75,14 +88,33 @@ int main()
 	//GL_STREAM_DRAW ：数据每次绘制时都会改变。
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	//设置顶点属性指针
+	/*第一个参数指定我们要配置的顶点属性。还记得我们在顶点着色器中使用layout(location = 0)定义了position顶点属性的位置值(Location)吗？它可以把顶点属性的位置值设置为0。
+		因为我们希望把数据传递到这一个顶点属性中，所以这里我们传入0。
+	第二个参数指定顶点属性的大小。顶点属性是一个vec3，它由3个值组成，所以大小是3。
+	第三个参数指定数据的类型，这里是GL_FLOAT(GLSL中vec * 都是由浮点数值组成的)。
+	第四个参数定义我们是否希望数据被标准化(Normalize)。如果我们设置为GL_TRUE，所有数据都会被映射到0（对于有符号型signed数据是 - 1）到1之间。我们把它设置为GL_FALSE。
+	第五个参数叫做步长(Stride)，它告诉我们在连续的顶点属性组之间的间隔。由于下个组位置数据在3个float之后，我们把步长设置为3 * sizeof(float)。
+		要注意的是由于我们知道这个数组是紧密排列的（在两个顶点属性之间没有空隙）我们也可以设置为0来让OpenGL决定具体步长是多少（只有当数值是紧密排列时才可用）。
+		一旦我们有更多的顶点属性，我们就必须更小心地定义每个顶点属性之间的间隔，我们在后面会看到更多的例子
+		（译注: 这个参数的意思简单说就是从这个属性第二次出现的地方到整个数组0位置之间有多少字节）。
+	最后一个参数的类型是void * ，所以需要我们进行这个奇怪的强制类型转换。它表示位置数据在缓冲中起始位置的偏移量(Offset)。
+	由于位置数据在数组的开头，所以这里是0。我们会在后面详细解释这个参数。*/
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
 	//创建顶点着色器
-	unsigned int vertexShader;
+	unsigned int vertexShader, fragmentShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
 	//附加着色器代码
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);
 	//编译顶点着色器代码
 	glCompileShader(vertexShader);
+	glCompileShader(fragmentShader);
 
 	//检测是否编译成功
 	int success;
@@ -94,6 +126,34 @@ int main()
 		std::cout << "顶点着色器编译失败：\n" << infoLog << std::endl;
 	}
 
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "片段着色器编译失败：\n" << infoLog << std::endl;
+	}
+
+	//创建程序对象
+	unsigned int shaderProgram = glCreateProgram();
+	//将着色器附加到程序对象上
+	glAttachShader(shaderProgram,vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	//链接着色器
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram,512,NULL,infoLog);
+		std::cout << "链接着色器失败！！" << infoLog << std::endl;
+	}
+
+	//使用着色器程序
+	glUseProgram(shaderProgram);
+
+	//删除着色器
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	//如果窗口没有被关闭则会循环渲染窗口
 	while (!glfwWindowShouldClose(window))
@@ -101,10 +161,16 @@ int main()
 		//按键监控
 		processInput(window);
 
-		//渲染指令TODO
-		
+		//渲染指令
 		glClearColor(0.2f,0.3f,0.3f,1.0f);	  //渲染颜色
-		glClear(GL_COLOR_BUFFER_BIT);//清除上一帧的颜色
+		glClear(GL_COLOR_BUFFER_BIT); //清除上一帧的颜色
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
+
+
+		glBindVertexArray(0);
 
 		//函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
 		glfwSwapBuffers(window);
